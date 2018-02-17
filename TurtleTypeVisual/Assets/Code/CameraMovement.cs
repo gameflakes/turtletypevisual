99e4f704935ed9d757727e67f2b8a8f1ce9dc048
Xchowks <correa.lucassr@gmail.com>
2018-02-17 02:09:21 -0200

    Turtle Type Visual
    Visual test

---
+++ TurtleTypeVisual/Assets/Code/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public GameObject followingObject;
+    public float moveSpeed;
+
+    private float yD;
+    private float zD;
+
+    private void Awake()
+    {
+        yD = transform.position.y;
+        zD = transform.position.z;
+    }
+
+	void FixedUpdate ()
+    {
+        Vector3 newPosition = followingObject.transform.position;
+        newPosition.y += yD;
+        newPosition.z += zD;
+        transform.position = Vector3.Lerp(transform.position, newPosition, moveSpeed * Time.deltaTime);
+	}
+}
---
+++ TurtleTypeVisual/Assets/Code/TurtleMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleMovement : MonoBehaviour {
+
+    private Rigidbody rgb;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rgb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        bool horT = false;
+        bool verT = false;
+
+        if ( hor > 0.5f || hor < -0.5f )
+        {
+            rgb.velocity = new Vector3(hor * moveSpeed, 0.0f, rgb.velocity.z);
+            horT = true;
+        }
+        else
+        {
+            rgb.velocity = new Vector3(0.0f, 0.0f, rgb.velocity.z);
+        }
+
+        if ( ver > 0.5f || ver < -0.5f)
+        {
+            rgb.velocity = new Vector3(rgb.velocity.x, 0.0f, ver * moveSpeed);
+            verT = true;
+        }
+        else
+        {
+            rgb.velocity = new Vector3(rgb.velocity.x, 0.0f, 0.0f);
+        }
+
+        if ( horT && verT )
+        {
+            rgb.velocity *= 0.7f;
+        }
+	}
+}
